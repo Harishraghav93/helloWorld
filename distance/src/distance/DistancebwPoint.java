0e0d5896fb5a30089cf0d837922f9eeaa81135b8
HP <HP@Harish.lan>
2019-12-12 12:16:04 +0100

    i have commited my first changes

---
+++ distance/src/distance/DistancebwPoint.java
@@ -0,0 +1,24 @@
+package distance;
+
+import java.lang.Math;
+public class DistancebwPoint
+{
+	public static void main(String arg[])	
+	{
+           /* int x1,x2,y1,y2;
+	    double dist;
+	    x1=0;y1=0;x2=1;y2=1;
+	    dist=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	 	    
+            System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dist);*/
+		
+		for(int i=0;i<=10;i++) {
+			System.out.println(+i+"," +i);
+			
+		}
+		
+		
+	}
+	
+	
+	
+}
---
+++ distance/src/distance/Main.java
@@ -0,0 +1,34 @@
+package distance;
+
+public class Main  {
+
+	public static void main(String[] args)  {
+
+		/*try {
+		Main.getNum();
+	}catch(Exception e) {
+	    System.out.println(e);
+		System.out.println("not executing");
+	}
+		finally {
+			System.out.println("it has to come in finally");
+		}
+	
+	}
+	
+	
+	public static void getNum() throws Exception {
+		int x=10;
+		try {
+		System.out.println("get the num   "+x/0);
+		}catch(Exception e) {
+			System.out.println("error has occured");
+			
+		}
+		throw new Exception();*/
+	System.out.println("Helloworld!");
+		
+		
+	}
+	
+}
